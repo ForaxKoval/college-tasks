e0da2af28a5be4c2174c7c6cf8b10b128940676c
ForaxKoval <faster7631@gmail.com>
2025-09-22 11:19:56 +0000

    Табуляція + завдання 3-4

--- FirstEX.cpp
+++ FirstEX.cpp
@@ -2,36 +2,52 @@
 #include <algorithm>
 #include <string>
 int main (){
-int a;
-int b;
-int sum = 0;
-std::cout << "Input numbers in A:";
-std::cin >> a;
-std::cout << "Input numbers in B:";
-std::cin >> b;
-std::cout << "A:" << a << ", B:" << b << std::endl;
+  int a;
+  int b;
+  int sum = 0;
+    std::cout << "ЗАВДАННЯ 1: " << std::endl;
+    std::cout << "Впишіть число яке буде A:";
+    std::cin >> a;
+    std::cout << "Впишіть число яке буде B:";
+    std::cin >> b;
+    std::cout << "A:" << a << ", B:" << b << std::endl;
 
 if (a % 2 !=0) a++;
-for (int i = a; i <= b; i += 2) { 
-sum += i; 
-}
-std::cout << "Suma parnih  chisel: " << sum << std::endl;
+  for (int i = a; i <= b; i += 2) { 
+  sum += i; }
+  std::cout << "Сума парних чисел: " << sum << std::endl;
 
-std::cout << "ZAVDANNYA 2: " << std::endl;
-int number0;
-std::cout << "Input number:";
-std::cin >> number0;
-if (number0 > 0){
-std::cout << "Chislo   " << number0 << "  Bilshe 0  " << std::endl;
-} else if (number0 < 0){
-std::cout << "Chislo   " << number0 << "  Menshe 0  " << std::endl;
-} else {
-std::cout << "Chislo   " << number0 << "  Ce 0  " << std::endl;
+std::cout << "ЗАВДАННЯ 2: " << std::endl;
+  int number0;
+  std::cout << "Впишіть число:";
+  std::cin >> number0;
+    if (number0 > 0){
+      std::cout << "Число  " << number0 << "  Більше 0  " << std::endl;
+      } else if (number0 < 0){
+      std::cout << "Число   " << number0 << "  Менше 0  " << std::endl;
+      } else {
+      std::cout << "Число   " << number0 << "  Це і є 0  " << std::endl;
+      }
+      
+std::cout << "ЗАВДАННЯ 3: " << std::endl;
+  const char * num = "Letters"; 
+  std::cout << "Наш рядок: " << num << std::endl;
+    int numofletters = 0;
+    int f;
+    for (f = 0; num[f] != '\0'; f++){
+      numofletters++;
+    }
+std::cout << "Кількість символів у рядку: " << numofletters << std::endl;
+
+std::cout << "ЗАВДАННЯ 4: " << std::endl;
+  int number = 707118;
+  std::string str = std::to_string(number);
+  std::cout << "Число як рядок: " << str << std::endl;
+    return 0;
+    
+
+
+
+
 }
-std::cout << "ZAVDANNYA 4: " << std::endl;
-int number-string;
-std::cout << "Input number:";
-std::cin >> number-string;
-std::string = std::to_string(number-string);
-return 0;
-}
+
