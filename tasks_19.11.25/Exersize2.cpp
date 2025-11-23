31b65266a44d4388ef846ee1e43a92d8da54b5c1
ForaxKoval <faster7631@gmail.com>
2025-11-23 11:05:50 +0000

    second ex done

--- tasks_19.11.25/Exersize2.cpp
+++ tasks_19.11.25/Exersize2.cpp
@@ -4,4 +4,10 @@
         std::cout << "Insert your words: " << std::endl; 
         std::string text;
         std::getline(std::cin, text);
+
+        int countlength = 0;
+        for (char letter : text){
+            countlength++;
+        }
+        std::cout << "string length:" << countlength << std::endl;
     }
