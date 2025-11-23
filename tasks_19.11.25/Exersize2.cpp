0ceeea8f08440443f57c4be35774ea111ae384a6
ForaxKoval <faster7631@gmail.com>
2025-11-23 10:57:47 +0000

    upd

---
+++ tasks_19.11.25/Exersize2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+    int main(){
+        std::cout << "Insert your words: " << std::endl; 
+        std::string text;
+        std::getline(std::cin, text);
+    }
