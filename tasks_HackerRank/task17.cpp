8e6b742e4822b5c07025389a21b3f78e37fcffbc
Forax <faster7631@gmail.com>
2025-12-12 13:46:45 +0000

    task done

--- tasks_HackerRank/task17.cpp
+++ tasks_HackerRank/task17.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 	int main(){
-        // 15. Print even numbers from 1 to n.
+        // 17. Calculate the sum of numbers from 1 to n.
         int n;
+        long int sum = 0;
         std::cout << "Insert the number to which the numbers will be output:" << std::endl;
         std::cin >> n;
         std::cout << "-----------------------------" << std::endl;
         for (int i = 0; i <= n; i++)
         {
-                std::cout << i << std::endl;
+                sum = sum + i;
         }
-        
+        std::cout << "Sum:" << sum << std::endl;
 
 }
 
