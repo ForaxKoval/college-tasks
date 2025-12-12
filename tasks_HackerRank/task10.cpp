427d255293492fefbfe925b86f83555c3af584f4
Forax <faster7631@gmail.com>
2025-12-12 10:43:43 +0000

    task done

---
+++ tasks_HackerRank/task10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+	int main(){
+        // 10. Calculate the area of a rectangle.
+        int a, b;
+        std::cout << "Instert length of first side:" << std::endl;
+        std::cin >> a;
+        std::cout << "Instert length of seccond side:" << std::endl;
+        std::cin >> b;
+
+        std::cout << "Area of the rectangle:" << a * b << std::endl;
+}
+
