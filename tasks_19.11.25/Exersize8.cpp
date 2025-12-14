df77f461a70a128865882c1a5d639c6727764740
Forax <faster7631@gmail.com>
2025-12-14 15:17:45 +0000

    8 task done..

--- tasks_19.11.25/Exersize8.cpp
+++ tasks_19.11.25/Exersize8.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <string>
 	int main(){
 		// SANDWICH WITH ICECREAM
-        bool IS_IT_SANDWICH? 
-		// ill be back to this ex soon...
+        std::string sandwich;
+		std::cout << "enter a row ->" << std::endl;
+		std::cin >> sandwich;
+
+		// count how mush symbols is
+
+		int numofletters = 0;
+    	for (int f = 0; sandwich[f] != '\0'; f++){
+      		numofletters++;
+    	}
+		std::cout << "we have " << numofletters << " symbols" << std::endl;
+
+		if (numofletters < 3)
+		{
+			std::cout << "Symbols must be more than 3 its not SANDWICH" << std::endl;
+			return 0;
+		}
+
+		int middlesymbol = 	numofletters / 2;
+		char middlesymbolChar = sandwich[middlesymbol];	
+
+		for (int i = 0; i < middlesymbol; i++)
+		{
+			if (sandwich[i] != sandwich [numofletters - 1 - i]) 
+			{
+				std::cout << "its NOT SANDWICH" << std::endl;
+				std::cout << "bye" << std::endl;
+				return 0;
+			}
+		}
+		std::cout << "our row: " << sandwich << " A SANDWICH YEEE" << std::endl;
+		
+		
+		
+
+		
 }
