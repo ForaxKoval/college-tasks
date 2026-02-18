3afd1b3f331ebc28761eeec556b5d15aa7ec0595
Forax <faster7631@gmail.com>
2026-02-18 12:44:39 +0000

    the eighth done

---
+++ YEAR_2026/tasks_09.02.26/task8.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+struct Product1{
+    std::string name;
+    double price;
+};
+struct Product2{
+    std::string name;
+    double price;
+};
+int main (){
+    Product1 product1;
+    Product2 product2;
+    
+    std::cout << "Enter product 1 name:   ";
+    getline(std::cin, product1.name);
+
+    std::cout << "Enter product 1 price:   ";
+    std::cin >> product1.price;
+
+    std::cin.ignore();
+
+    std::cout << "Enter product 2 name:   ";
+    getline(std::cin, product2.name);
+
+    std::cout << "Enter product 2 price:   ";
+    std::cin >> product2.price;
+
+    if (product1.price > product2.price)
+    {
+        std::cout << product1.name << " is more expensive" << std::endl;
+    }else if (product1.price < product2.price)
+    {
+        std::cout << product2.name << " is more expensive" << std::endl;
+    }else{
+        std::cout << "the prices are the same" << std::endl;
+    }
+
+    
+}
