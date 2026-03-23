05e59b63baec79ea4afce15f2f799842820d9321
Forax <faster7631@gmail.com>
2026-03-23 12:58:30 +0000

    testcode

---
+++ YEAR_2026/tasks_dynamic_arrays/tasktest.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <cassert>
+
+struct int_vector {
+    int* data;
+    int size;
+    int capacity;
+
+    int_vector(int initial_capacity = 4) {
+        data = new int[initial_capacity];
+        size = 0;
+        capacity = initial_capacity;
+    }
+
+    ~int_vector() {
+        delete[] data;
+    }
+
+    void resize(int new_capacity) {
+        int* new_data = new int[new_capacity];
+        int to_copy = (size < new_capacity) ? size : new_capacity;
+        for (int i = 0; i < to_copy; ++i) {
+            new_data[i] = data[i];
+        }
+        delete[] data;
+        data = new_data;
+        capacity = new_capacity;
+        size = to_copy;
+    }
+
+    void push_back(int value) {
+        if (size == capacity) {
+            resize(capacity * 2);
+        }
+        data[size] = value;
+        ++size;
+    }
+
+    int& at(int index) {
+        assert(index >= 0 && index < size);
+        return data[index];
+    }
+
+    int& operator[](int index) {
+        return at(index);
+    }
+
+    void print() {
+        std::cout << "[";
+        for (int i = 0; i < size; ++i) {
+            if (i > 0) std::cout << ", ";
+            std::cout << data[i];
+        }
+        std::cout << "] (size=" << size
+                  << ", capacity=" << capacity << ")" << std::endl;
+    }
+
+    void sort(){
+        for (int j = 0; j < size - 1; j++)
+        {
+            bool swapped = false;
+            for (int i = 0; i < size - 1 - j; i++)
+            {
+                if (data[i] > data[i + 1])
+                {
+                    int temp = data[i];
+                    data[i] = data[i + 1];
+                    data[i + 1] = temp;
+                    swapped = true;
+                }
+                
+            }
+            if(!swapped){
+                break;
+            }
+        }
+        
+    }
+    int find(int value){
+        for (int i = 0; i < size; i++)
+        {
+            if (data[i] == value)
+            {
+                return i;
+            }
+            
+        }
+        return -1;
+        
+    }
+};
+
+
+int main() {
+    int_vector v;
+
+    for(int i = 100; i > 0; i -=10){
+        v.push_back(i);
+    }
+    v[3] = 999;
+    v.print(); 
+    
+
+
+    v.sort();
+    v.print();
+
+
+    std::cout << v.find(999) << std::endl;
+    std::cout << v.find(50) << std::endl;
+    std::cout << v.find(9000) << std::endl;
+
+
+    return 0;
+}
