eada6109426075387fb7d9c67b88c83494108255
Forax <faster7631@gmail.com>
2026-06-04 08:01:31 +0000

    process

---
+++ YEAR_2026_PYTHON_PRACTICE/task3-8.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+bool checkPassword(std::string password)
+{
+    return password == "7631";
+}
+
+int main()
+{
+    std::string password;
+    int attempts = 0;
+    int maxAttempts = 3;
+
+    while (attempts < maxAttempts)
+    {
+        std::cout << "Vvedit parol: ";
+        std::cin >> password;
+
+        if (checkPassword(password))
+        {
+            std::cout << "Dostup dozvoleno!";
+            return 0;
+        }
+            attempts++;
+            std::cout << "Nevirnyi parol. Sprob: "
+                  << attempts << "/" << maxAttempts << std::endl;
+    }
+
+    std::cout << "Dostup zablokovano!";
+
+    return 0;
+}
